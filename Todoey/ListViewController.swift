56d73e5a1fdd40d8afea5c8b0164077b90dd3be9
Sohit Shab <macbook@apples-macbook-air.local>
2018-04-25 04:42:14 +0530

    ToDoey list complete with DataSource and Delegate method

---
+++ Todoey/ListViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by apple on 24/04/18.
+//  Copyright © 2018 apple. All rights reserved.
+//
+
+import UIKit
+
+class ListViewController: UITableViewController {
+
+    let fruitsName = ["apple","bnana","orange","gava","pomegarnate","gava","apple","bnana","orange","apple","bnana","orange","apple","bnana","orange","apple","bnana","orange","gava","pomegarnate","gava","apple","bnana","orange","apple","bnana","orange","apple","bnana","orange" ]
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    //DataSource method
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return fruitsName.count
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PassData", for: indexPath)
+        cell.textLabel?.text = fruitsName[indexPath.row]
+        return cell
+    }
+    //delegate method
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        //print(fruitsName[indexPath.row])
+        
+//        tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+//        
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark{
+        
+             tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        }else{
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+
+    
+}
+
